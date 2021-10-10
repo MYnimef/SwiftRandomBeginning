49db811719830d83e8a9b92e5358f340bf9e63de
Ivan <ivan.markov.2013@gmail.com>
2021-10-11 01:21:11 +0300

    1st lesson hw

---
+++ TaskFirstLesson/TaskFirstLesson/main.swift
@@ -0,0 +1,9 @@
+var str: String = "Swift"
+var ch: Character = "S"
+var mass: [String] = ["S", "w", "i", "f", "t"]
+
+var dict1: Dictionary = ["One": (100,101), "Two": (200,201)]
+var dict4 = Dictionary(uniqueKeysWithValues: [(1,2), (3,4), (5,6)])
+var dict5 = ["Alex": 1989, "John": 2002, "Harold": 1951]
+
+//Array
